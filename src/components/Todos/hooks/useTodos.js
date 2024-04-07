a28b1253000f79da9079bf44c03039ab00ddf524
robotrainer <asbu26@gmail.com>
2024-04-07 11:56:02 +0300

    connect api, added change status function

--- src/components/Todos/hooks/useTodos.js
+++ src/components/Todos/hooks/useTodos.js
@@ -27,20 +27,57 @@ export default function useTodos() {
     }
   };
 
-  const addTodo = (todo) => {
-    if (todo) {
-      setTodos((todos) => [...todos, { id: Date.now(), text: todo }]);
+  const addTodo = async (todo) => {
+    try {
+      const todoObj = { id: JSON.stringify(Date.now()), text: todo };
+      const response = await fetch("http://localhost:3001/todos", {
+        method: "POST",
+        body: JSON.stringify(todoObj),
+      });
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      setTodos((todos) => [...todos, data]);
+    } catch (error) {
+      console.error(error);
+      // setError(error);
     }
   };
 
-  const cleanTodos = () => {
+  const cleanTodos = async () => {
     setTodos([]);
+  };
+
+  const changeStatusTodo = async (todo) => {
+    try {
+      const changedStatus = { completed: !todo.completed };
+      const response = await fetch(`http://localhost:3001/todos/${todo.id}`, {
+        method: "PATCH",
+        body: JSON.stringify(changedStatus),
+      });
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      setTodos((todos) =>
+        todos.map((elem) => {
+          return elem.id == data.id ? data : elem;
+        })
+      );
+    } catch (error) {
+      console.error(error);
+      // setError(error);
+    }
   };
 
   return {
     todos,
     addTodo,
     cleanTodos,
+    changeStatusTodo,
     isLoading,
     isError: error != null,
     error,
