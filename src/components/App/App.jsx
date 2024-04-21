92c4d839a37a6d39d353820914360cf4fa985a3a
robotrainer <asbu26@gmail.com>
2024-04-21 11:46:53 +0300

    small fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { TodosUncontrolled } from "../Todos/TodosUncontrolled/TodosUncontrolled";
 import { TodosControlled } from "../Todos/TodosControlled/TodosControlled";
 
